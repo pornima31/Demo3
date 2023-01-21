20153ac1e7b8933b730bdc652e53f37626374497
pornima31 <pornima31hadap@gmail.com>
2023-01-22 01:23:24 +0530

    arraylist demo

---
+++ src/com/velocity/Code3.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Code3 {
+	public static void main(String[] args) {
+	ArrayList<Integer>al = new ArrayList<Integer>();
+	al.add(10);
+	al.add(20);
+	al.add(30);
+	for(int i: al) {
+		System.out.println("" +i);
+	}
+		
+		
+		
+}
+
+}
